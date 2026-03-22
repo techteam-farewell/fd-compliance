dbef27be7764357a5c921e582324673abfa721aa
techteam-farewell <techteam@thefarewellguide.co.uk>
2026-03-22 13:55:24 +0100

    Updateed The startup.sh File

--- startup.sh
+++ startup.sh
@@ -2,6 +2,11 @@
 
 cd /home/site/wwwroot
 
+# Ensure required folders exist
 mkdir -p storage bootstrap/cache
+
+# Set correct permissions
 chmod -R 775 storage bootstrap/cache
 chmod -R 755 public
+
+echo "Startup script finished."
