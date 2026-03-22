3228768e99b39fa64a48c30d0e40df6e2075cdd9
techteam-farewell <techteam@thefarewellguide.co.uk>
2026-03-22 15:10:49 +0100

    Updated startup.sh File

--- startup.sh
+++ startup.sh
@@ -2,12 +2,19 @@
 
 cd /home/site/wwwroot
 
-# Fix permissions
+echo "Fixing Laravel structure..."
+
+# Ensure required folders
 mkdir -p storage bootstrap/cache
+
+# Fix permissions
 chmod -R 775 storage bootstrap/cache
 chmod -R 755 public
 
-# Force correct document root by copying public contents to root
+# Remove default Azure page
+rm -f hostingstart.html
+
+# Copy Laravel public files to root (IMPORTANT)
 cp -r public/* /home/site/wwwroot/
 
-echo "Startup script finished."
+echo "Startup complete."
