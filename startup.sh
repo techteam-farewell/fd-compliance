ef4a23e4be597e01e4ffbd1b4a67124fbc8f00b4
techteam-farewell <techteam@thefarewellguide.co.uk>
2026-03-26 06:38:33 +0100

    Updated Startup.sh

--- startup.sh
+++ startup.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
+echo "STARTUP SCRIPT IS RUNNING" > /home/site/wwwroot/test.txt
+
 cd /home/site/wwwroot
 
-echo "Installing dependencies..."
-composer install --no-dev --optimize-autoloader
-
-echo "Setting permissions..."
-chmod -R 775 storage
-chmod -R 775 bootstrap/cache
-
-echo "Preparing Laravel..."
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-echo "Moving public files to root..."
 cp -r public/. /home/site/wwwroot/
-
-echo "Done!"
